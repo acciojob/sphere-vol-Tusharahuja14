099edf49a76432a100e9e8693aa1da54a6e0d414
Tushar Ahuja <93457512+Tusharahuja14@users.noreply.github.com>
2025-01-19 13:17:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-function volume_sphere() {
+function volume_sphere(event) {
     //Write your code here
+	event.preventDefault();
+	let radius=document.getElementById("radius");
+	let rval=radius.value;
+	let volume=document.getElementById("volume");
+			if(isNaN(rval)||rval<0)
+			{
+				alert("NaN");
+			}
+			else
+			{let pi=Math.PI;
+				let res=4/3*(pi*(rval*rval*rval));
+			 volume.value=res.toFixed(2);
+			}
   
 } 
 
